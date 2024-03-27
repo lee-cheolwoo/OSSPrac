e16396b414c3459276ed7511f100e4bc262b4389
lee-cheolwoo <cjf4674@dgu.ac.kr>
2024-03-27 17:31:04 +0900

    2024-03-27 개인과제5 6번

--- Psub5/homework.py
+++ Psub5/homework.py
@@ -1,4 +1,5 @@
 name = input('이름을 입력하세요: ')
 number = input('학번을 입력하세요: ')
-print(f"<출력>\n이름: {name}\n학번: {number}")
+major = input('학과를 입력하세요: ')
+print(f"\n<출력>\n이름: {name}\n학번: {number}\n학과: {major}")
 
